6c0fecebea0d0c54731c02b169f108b17d33ba1e
matiasscs1 <matias.cedeno@udla.edu.ec>
2024-05-09 18:35:43 -0500

    nuevo cambio  mayo 5

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-  
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    // Otros plugins si los tienes
+  ],
+  build: {
+    rollupOptions: {
+      external: ['react-apexcharts'] // Si necesitas externalizar algún módulo
+    }
+  }
+});
