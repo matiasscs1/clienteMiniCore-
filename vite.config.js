48630d7c716249975580fe787f5caa3edec7e039
matiasscs1 <matias.cedeno@udla.edu.ec>
2024-06-15 14:52:50 -0500

    Descripción core 2

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import commonjs from '@rollup/plugin-commonjs'
 
 export default defineConfig({
   plugins: [
     react(),
+    commonjs(), // Añadir este plugin
     // Otros plugins si los tienes
   ],
-  build: {
-    rollupOptions: {
-      external: ['jspdf'],
-    },
-  },
 });
